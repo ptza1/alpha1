6318bce6089a1502f9eb70d8b64bf3c0a47cca94
Toatreezel <toatreezel@gmail.com>
2020-03-04 13:05:56 -0300

    Versao 1
    feito na Unity

---
+++ Assets/Scripts/Teste.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    public float velocidade = 5;
+
+    Rigidbody fisica;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fisica = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movimentoVertical = Input.GetAxis("Vertical");
+
+        fisica.velocity = movimentoVertical * velocidade * transform.forward;
+
+        float movimentoHorizontal = Input.GetAxis("Horizontal");
+
+        transform.Rotate(0, movimentoHorizontal * velocidade, 0);
+    }
+}
---
+++ Assets/Scripts/inimigoSeguindo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class inimigoSeguindo : MonoBehaviour
+{
+    NavMeshAgent agente;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.SetDestination(player.position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Voce morreu");
+        }
+    }
+}
