dc8a68fe7d280391711328bf280ad4f86f83c77f
Toatreezel <toatreezel@gmail.com>
2020-03-05 22:27:28 -0300

    primeira do projeto recriado

---
+++ Assets/Scripts/inimigoSeguindo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class inimigoSeguindo : MonoBehaviour
+{
+    NavMeshAgent agente;
+    public Transform player;
+    float offset = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Vector3.Distance(player.position, this.transform.position) >= offset)
+        {
+            agente.SetDestination(player.position);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Voce morreu");
+        }
+    }
+}
---
+++ Assets/Scripts/movimentacao.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentacao : MonoBehaviour
+{
+    public float Speed;
+    Rigidbody fisica;
+
+    private void Start()
+    {
+        fisica = GetComponent<Rigidbody>();
+
+    }
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        /*Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        * transform.Translate(playerMovement, Space.Self); */
+        Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized * Speed * Time.deltaTime;
+        fisica.MovePosition(transform.position + playerMovement);
+    }
+}
